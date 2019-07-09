1ea7a1e297fbabf0df5285392a71f86f7b5d5c9e
Eduard <etrayt@allmax.team>
2019-07-09 09:27:56 +0700

    Imagemin changes

--- config/webpack.build.config.js
+++ config/webpack.build.config.js
@@ -2,13 +2,14 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const ImageminWebpack = require('imagemin-webpack');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const pngquant = require('imagemin-pngquant');
-const svgo = require('imagemin-svgo');
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminOptipng = require('imagemin-optipng');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminSvgo = require('imagemin-svgo');
 const baseWebpackConfig = require('./webpack.base.config');
 
 const CssExtractPlugin = new MiniCssExtractPlugin({
@@ -16,90 +17,109 @@ const CssExtractPlugin = new MiniCssExtractPlugin({
   chunkFilename: 'css/[id].css',
 });
 
-const ImageminPlugin1 = new ImageminPlugin({
-  test: /\.(jpe?g|png|gif|svg)$/i,
-  optipng: null,
-  jpegtran: null,
-  svgo: null,
-  plugins: [
-    imageminMozjpeg({
-      quality: 90,
-      progressive: true,
-      fastCrush: false, /* default false */
-      dcScanOpt: 1, /* 1...3 default 1 */
-      trellis: true, /* default true */
-      tune: 'hvs-psnr', /* Trellis optimization method. Available: psnr, hvs-psnr(default), ssim, ms-ssim */
-      trellisDC: true, /* default true */
-      overshoot: true, /* default true */
-      arithmetic: false, /* default false */
-      dct: 'int', /* Available: int(default), fast, float */
-    }),
-    pngquant({
-      quality: [0.3, 0.5],
-      speed: 3, /* 1...11 default 3 */
-      strip: false, /* default false */
-      dithering: 1, /* 0...1 default 1 */
-      posterize: 0, /* default 0 */
-      verbose: false, /* default false */
-    }),
-    svgo({
-      plugins: [
-        { removeComments: true },
-        { removeXMLProcInst: true },
-      ],
-    }),
-  ],
+const ImageminPluginLossless = new ImageminWebpack({
+  // When cache = true, use "rm -rf ./node_modules/.cache/imagemin-webpack" if change options for imagemin plugin!
+  test: /.(jpe?g|png|gif|svg)$/i,
+  cache: false,
+  bail: false,
+  loader: false,
+  name: 'images/content/[name].[ext]',
+  imageminOptions: {
+    plugins: [
+      imageminJpegtran({
+        progressive: true, /* default false */
+        arithmetic: true, /* default false */
+      }),
+      imageminOptipng({
+        optimizationLevel: 3, /* 0...7 default 3 */
+        bitDepthReduction: true, /* default true */
+        colorTypeReduction: true, /* default true */
+        paletteReduction: true, /* default true */
+      }),
+      imageminSvgo({
+        plugins: [
+          { removeComments: true },
+          { removeXMLProcInst: true },
+        ],
+      }),
+      imageminGifsicle({
+        interlaced: true, /* default false */
+        optimizationLevel: 1, /* 1...3 default 1 */
+        colors: 256, /* 2...256, max colors in img */
+      }),
+    ],
+  },
+  filter: (source, sourcePath) => {
+    if (/png\.webp/.test(sourcePath)) {
+      return false;
+    }
+
+    if (/decoration/.test(sourcePath)) {
+      return false;
+    }
+
+    return true;
+  },
 });
 
-// const ImageminPlugin = new ImageminWebpack({
-//   // When cache = true, use "rm -rf ./node_modules/.cache/imagemin-webpack" if change options for imagemin plugin!
-//   test: /.(jpe?g|png|gif|svg|webp)$/i,
-//   cache: false,
-//   bail: false,
-//   loader: false,
-//   name: 'images/[name].[ext]',
-//   imageminOptions: {
-//     plugins: [
-//       imageminMozjpeg({
-//         quality: 90,
-//         progressive: true,
-//         fastCrush: false, /* default false */
-//         dcScanOpt: 1, /* 1...3 default 1 */
-//         trellis: true, /* default true */
-//         tune: 'hvs-psnr', /* Trellis optimization method. Available: psnr, hvs-psnr(default), ssim, ms-ssim */
-//         trellisDC: true, /* default true */
-//         overshoot: true, /* default true */
-//         arithmetic: false, /* default false */
-//         dct: 'int', /* Available: int(default), fast, float */
-//       }),
-//       pngquant({
-//         quality: [0.3, 0.5],
-//         speed: 3, /* 1...11 default 3 */
-//         strip: false, /* default false */
-//         dithering: 1, /* 0...1 default 1 */
-//         posterize: 0, /* default 0 */
-//         verbose: false, /* default false */
-//       }),
-//       svgo({
-//         plugins: [
-//           { removeComments: true },
-//           { removeXMLProcInst: true },
-//         ],
-//       }),
-//     ],
-//   },
-//   filter: (source, sourcePath) => {
-//     if (/png\.webp/.test(sourcePath)) {
-//       return false;
-//     }
+const ImageminPluginLossy = new ImageminWebpack({
+  // When cache = true, use "rm -rf ./node_modules/.cache/imagemin-webpack" if change options for imagemin plugin!
+  test: /.(jpe?g|png|gif|svg)$/i,
+  cache: false,
+  bail: false,
+  loader: false,
+  name: 'images/decoration/[name].[ext]',
+  imageminOptions: {
+    plugins: [
+      imageminMozjpeg({
+        quality: 90,
+        progressive: true,
+        fastCrush: false, /* default false */
+        dcScanOpt: 1, /* 1...3 default 1 */
+        trellis: true, /* default true */
+        tune: 'hvs-psnr', /* Trellis optimization method. Available: psnr, hvs-psnr(default), ssim, ms-ssim */
+        trellisDC: true, /* default true */
+        overshoot: true, /* default true */
+        arithmetic: false, /* default false */
+        dct: 'int', /* Available: int(default), fast, float */
+      }),
+      imageminPngquant({
+        quality: [0.3, 0.5],
+        speed: 3, /* 1...11 default 3 */
+        strip: false, /* default false */
+        dithering: 1, /* 0...1 default 1 */
+        posterize: 0, /* default 0 */
+        verbose: false, /* default false */
+      }),
+      imageminSvgo({
+        plugins: [
+          { removeComments: true },
+          { removeXMLProcInst: true },
+        ],
+      }),
+      imageminGifsicle({
+        interlaced: true, /* default false */
+        optimizationLevel: 1, /* 1...3 default 1 */
+        colors: 256, /* 2...256, max colors in img */
+      }),
+    ],
+  },
+  filter: (source, sourcePath) => {
+    if (/png\.webp/.test(sourcePath)) {
+      return false;
+    }
 
-//     return true;
-//   },
-// });
+    if (/content/.test(sourcePath)) {
+      return false;
+    }
+
+    return true;
+  },
+});
 
 const ImageminWebpPlugin = new ImageminWebpWebpackPlugin({
   config: [{
-    test: /\.(jpeg|jpg|png)/,
+    test: /\.(jpe?g|png)/,
     options: {
       preset: 'default', /* default, photo, picture, drawing, icon, text */
       quality: 90,
@@ -122,7 +142,8 @@ const plugins = [
   new CleanWebpackPlugin(),
   CssExtractPlugin,
   ImageminWebpPlugin,
-  ImageminPlugin1,
+  ImageminPluginLossless,
+  ImageminPluginLossy,
 ];
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
